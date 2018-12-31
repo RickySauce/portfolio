cce09ee08c290711c7b6d2b349fc730b3a03ec22
learn <eric.desimone@cortland.edu>
2018-12-31 12:19:38 -0500

    updated

--- src/components/navbar/NavMenu.js
+++ src/components/navbar/NavMenu.js
@@ -2,21 +2,25 @@ import React from 'react';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import '../../css/technology-icons.css'
-import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import { Menu, MenuItem, IconButton } from 'react-mdl'
 import { MdMenu } from "react-icons/md";
 
 const NavMenu = (props) => {
 
+  const handleClick = (event) => {
+    props.history.push(event.currentTarget.dataset.href)
+  }
+
   return (
     <div id='menu-div'>
       <Menu className='menu-links' target="menu">
-          <MenuItem><Link to='/aboutme'>About Me</Link></MenuItem>
-          <MenuItem><Link to='/projects'>Projects</Link></MenuItem>
+          <MenuItem onClick={handleClick} data-href='/aboutme'> About Me </MenuItem>
+          <MenuItem onClick={handleClick} data-href='/projects' > Projects </MenuItem>
           <MenuItem onClick={props.handleContact}>Contact</MenuItem>
       </Menu>
   </div>
   );
 }
 
-export default NavMenu;
+export default withRouter(NavMenu);
